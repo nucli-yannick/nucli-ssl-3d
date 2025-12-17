bbae3a7e6faa21987d915af878b7683c0cccc5b0
Yannick Brackenier <yannick.brackenier@nuclivision.com>
2025-12-17 11:51:26 +0100

    Fixed relative paths

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 requires = ['torch>=1.9', 'torchvision', 'nibabel', 'numpy', 'scipy', 'scikit-image','tqdm', 'matplotlib', 'seaborn', 'torchmetrics', 'onnx', 'mlflow', 'indexed-gzip', 'blosc2', 'monai']
 setup(
-    name="nucli_train",  # Just a placeholder name
+    name="ssl-3d",  # Just a placeholder name
     version="0.1.0",
     packages=find_packages('src'),
     package_dir={'': 'src'},

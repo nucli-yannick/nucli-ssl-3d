8e81b3748513cb7fd644a2d01d0161a6606f136c
Yannick Brackenier <yannick.brackenier@nuclivision.com>
2025-12-17 12:04:09 +0100

    Fixing env

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 from setuptools import setup, find_packages
 
-
 requires = ['torch>=1.9', 'torchvision', 'nibabel', 'numpy', 'scipy', 'scikit-image','tqdm', 'matplotlib', 'seaborn', 'torchmetrics', 'onnx', 'mlflow', 'indexed-gzip', 'blosc2', 'monai']
 setup(
-    name="ssl-3d",  # Just a placeholder name
+    name="ssl-3d", 
     version="0.1.0",
     packages=find_packages('src'),
     package_dir={'': 'src'},
